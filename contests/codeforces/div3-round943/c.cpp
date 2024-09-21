1f1b3acb270778134c2034f91988296e2f75426e
lira-frts <lira.frts@gmail.com>
2024-09-21 15:49:12 -0300

    diretorio nome errado

--- contests/codeforces/div3-round943/c.cpp
+++ contests/codeforces/div3-round943/c.cpp
@@ -12,12 +12,8 @@ void solve(){
     ll n; cin >> n;
     vector<ll> x(n-1), a(n), q(n);
     for(ll i = 0; i < n-1; i++) cin >> x[i];
-    a[0] = x[0]+1;
-    for(ll i = 1; i < n-1; i++){
-        q[i-1] = ((x[i] + 1 - x[i-1]) + a[i-1] - 1) / a[i-1];
-        a[i] = a[i-1]*q[i-1]+x[i-1];
-    }
-    a[n-1] = a[n-2] + x[n-2];
+    a[0] = 501;
+    for(ll i = 1; i < n; i++) a[i] = x[i-1] + a[i-1];
     for(ll i = 0; i < n; i++) cout << a[i] << ' '; cout << '\n';
 }
 
